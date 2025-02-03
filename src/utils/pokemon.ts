95bbbf48f797dd0fa8f56e1fd465f079e954f4d3
Gustavo Bays <bagy@MacBook-Air-de-Bagy.local>
2025-02-03 18:27:15 -0300

    feat: criado helper para buscar nome do atributo do pokemon com base no enum

--- src/utils/pokemon.ts
+++ src/utils/pokemon.ts
@@ -1,10 +1,14 @@
 import {
+  PokeApiPokemonStat,
+  PokeApiPokemonStatTypeEnum,
   PokeApiPokemonType,
   PokeApiPokemonTypeEnum,
 } from "../services/poke-api/types";
 import {
+  ATTACK,
   BUG,
   DARK,
+  DEFENSE,
   DRAGON,
   ELECTRIC,
   FAIRY,
@@ -14,16 +18,21 @@ import {
   GHOST,
   GRASS,
   GROUND,
+  HP,
   ICE,
   NORMAL,
   POISON,
   PSYCHIC,
   ROCK,
+  SPECIAL_ATTACK,
+  SPECIAL_DEFENSE,
+  SPEED,
   STEEL,
+  TOTAL,
   WATER,
 } from "../@constants/colors";
 
-export function getColorBy(type: PokeApiPokemonType): string {
+export function getColorByType(type: PokeApiPokemonType): string {
   const colors: Record<PokeApiPokemonTypeEnum, string> = {
     [PokeApiPokemonTypeEnum.GRASS]: GRASS,
     [PokeApiPokemonTypeEnum.FIRE]: FIRE,
@@ -46,4 +55,32 @@ export function getColorBy(type: PokeApiPokemonType): string {
   };
 
   return colors[type.type.name] || NORMAL;
+}
+
+export function getColorByStat(stat: PokeApiPokemonStat): string {
+  const colors: Record<PokeApiPokemonStatTypeEnum, string> = {
+    [PokeApiPokemonStatTypeEnum.HP]: HP,
+    [PokeApiPokemonStatTypeEnum.ATTACK]: ATTACK,
+    [PokeApiPokemonStatTypeEnum.DEFENSE]: DEFENSE,
+    [PokeApiPokemonStatTypeEnum.SPECIAL_ATTACK]: SPECIAL_ATTACK,
+    [PokeApiPokemonStatTypeEnum.SPECIAL_DEFENSE]: SPECIAL_DEFENSE,
+    [PokeApiPokemonStatTypeEnum.SPEED]: SPEED,
+    [PokeApiPokemonStatTypeEnum.TOTAL]: TOTAL
+  }
+
+  return colors[stat.stat.name] || NORMAL
+}
+
+export function getTextByStat(stat: PokeApiPokemonStat): string {
+  const texts: Record<PokeApiPokemonStatTypeEnum, string> = {
+    [PokeApiPokemonStatTypeEnum.HP]: 'HP',
+    [PokeApiPokemonStatTypeEnum.ATTACK]: 'ATK',
+    [PokeApiPokemonStatTypeEnum.DEFENSE]: 'DEF',
+    [PokeApiPokemonStatTypeEnum.SPECIAL_ATTACK]: 'STK',
+    [PokeApiPokemonStatTypeEnum.SPECIAL_DEFENSE]: 'SDF',
+    [PokeApiPokemonStatTypeEnum.SPEED]: 'SPD',
+    [PokeApiPokemonStatTypeEnum.TOTAL]: 'TOT'
+  }
+
+  return texts[stat.stat.name]
 }
