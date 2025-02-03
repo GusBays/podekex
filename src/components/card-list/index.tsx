48f80afe565147c76e301dad524a8cffa43af654
Gustavo Bays <bagy@MacBook-Air-de-Bagy.local>
2025-02-03 18:26:56 -0300

    feat: desabilitar botão quando estiver com estado loading = trie

--- src/components/card-list/index.tsx
+++ src/components/card-list/index.tsx
@@ -5,10 +5,12 @@ import { PokeApiPokemon, PokeApiPokemonType, PokeApiPokemonTypeEnum } from "../.
 import { pokeApiService } from "../../services/poke-api"
 import { POKE_API_IMAGE_URL } from "../../@constants/images"
 import Pill from "../pill"
-import { getColorBy } from "../../utils/pokemon"
+import { getColorByType } from "../../utils/pokemon"
+import { useLoading } from "../../providers/loader"
 
 const CardList: React.FC<CardListProps> = ({ id, onClick }) => {
     const [pokemon, setPokemon] = useState<PokeApiPokemon | null>(null)
+    const { loading } = useLoading()
 
     const getPokemon = async () => {
         if (pokemon) return
@@ -20,13 +22,13 @@ const CardList: React.FC<CardListProps> = ({ id, onClick }) => {
     })
 
     const toRenderTypes = (type: PokeApiPokemonType) =>  {
-        const color = getColorBy(type)
+        const color = getColorByType(type)
         const key = `${pokemon?.id}-${type.type.name}`
         return <Pill key={key} text={type.type.name} color={color} />
     }
 
     return (
-        <button className="card-list" onClick={() => pokemon ? onClick(pokemon) : null}>
+        <button className="card-list" onClick={() => pokemon ? onClick(pokemon) : null} disabled={loading}>
             <img className="image" src={`${POKE_API_IMAGE_URL}/${id}.png`} />
             {pokemon &&
                 <div className="card-info">
