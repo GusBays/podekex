45228c38b94af34b95a366f5df17108f8bb697b2
Gustavo Bays <bagy@MacBook-Air-de-Bagy.local>
2025-02-03 18:27:38 -0300

    feat: incluído restante dos dados, estilos e funcionalidades no card-detail, com evoluções, atributos e ajustes de estilo

--- src/components/card-detail/index.tsx
+++ src/components/card-detail/index.tsx
@@ -1,26 +1,29 @@
-import React, { useEffect, useState } from "react"
+import React, { ReactNode, useEffect, useState } from "react"
 import './styles.scss'
 import { CardDetailProps } from "./types"
-import { PokeApiPokemonAbility, PokeApiPokemonEvolutionChain, PokeApiPokemonSpecie, PokeApiPokemonSpecieFlavorText, PokeApiPokemonType } from "../../services/poke-api/types"
+import { PokeApiPokemonAbility, PokeApiPokemonEvolutionChain, PokeApiPokemonEvolutionChainChain, PokeApiPokemonSpecie, PokeApiPokemonSpecieFlavorText, PokeApiPokemonStat, PokeApiPokemonType } from "../../services/poke-api/types"
 import { pokeApiService } from "../../services/poke-api"
 import { POKE_API_IMAGE_URL } from "../../@constants/images"
-import { getColorBy } from "../../utils/pokemon"
+import { getColorByStat, getColorByType, getTextByStat } from "../../utils/pokemon"
 import Pill from "../pill"
 
-const CardDetail: React.FC<CardDetailProps> = ({ pokemon }) => {
+const CardDetail: React.FC<CardDetailProps> = ({ pokemon, onClickEvolution }) => {
     const [specie, setSpecie] = useState<PokeApiPokemonSpecie | null>(null)
     const [evolutionChain, setEvolutionChain] = useState<PokeApiPokemonEvolutionChain | null>(null)
 
     const getData = async () => {
         if (!pokemon) return
 
-        const [resSpecie, resEvolutionChain] = await Promise.all([
-            pokeApiService.getSpecie(pokemon.id),
-            pokeApiService.getEvolutionChain(pokemon.id)
-        ])
+        const res = await pokeApiService.getSpecie(pokemon.id)
+        setSpecie(res)
 
-        setSpecie(resSpecie)
-        setEvolutionChain(resEvolutionChain)
+        if (!res) return
+
+        const evolutionChainId = res.evolution_chain.url.match(/\/(\d+)\/$/)?.at(1)
+
+        if (!evolutionChainId) return
+
+        setEvolutionChain(await pokeApiService.getEvolutionChain(+evolutionChainId))
     }
 
     useEffect(() => {
@@ -28,7 +31,7 @@ const CardDetail: React.FC<CardDetailProps> = ({ pokemon }) => {
     }, [pokemon])
 
     const toRenderTypes = (type: PokeApiPokemonType) =>  {
-        const color = getColorBy(type)
+        const color = getColorByType(type)
         const key = `${pokemon?.id}-${type.type.name}`
         return <Pill key={key} text={type.type.name} color={color} />
     }
@@ -37,14 +40,51 @@ const CardDetail: React.FC<CardDetailProps> = ({ pokemon }) => {
 
     const toRenderAbilities = (ability: PokeApiPokemonAbility) => {
         const key = `${pokemon?.id}-${ability.ability.name}`
-        return <Pill key={key} text={ability.ability.name} color={'#a9a9a9'} />
+        return <Pill key={key} text={ability.ability.name} color={'#F6F8FC'} />
+    }
+
+    const toRenderStats = (stat: PokeApiPokemonStat) => {
+        const key = `${pokemon?.id}-${stat.stat.name}`
+        return (
+            <div className="stat">
+                <Pill key={key} text={getTextByStat(stat)} color={getColorByStat(stat)} rounded={true} />
+                <span>{stat.base_stat.toString()}</span>
+            </div>
+        )
+    }
+
+    const toRenderEvolution = (evolution: PokeApiPokemonEvolutionChainChain): ReactNode => {
+        const id = evolution.species.url.match(/\/(\d+)\/$/)?.at(1)
+
+        if (!id) return null
+
+        const key = `${id}-${evolution.species.name}`
+        const details = evolution.evolution_details[0]
+        const nextEvolution = evolution.evolves_to
+
+        return (
+            /** @ts-ignore */
+            <>
+                <div className="card-evolution" key={key}>
+                    {details?.min_level && 
+                        <Pill text={details?.min_level.toString()} color={'#F6F8FC'} />
+                    }
+                    <button onClick={async () => await onClickEvolution(+id)} disabled={!id || +id === pokemon?.id}>
+                        <img src={`${POKE_API_IMAGE_URL}/${id}.png`} />
+                    </button>
+                </div>
+                {nextEvolution && nextEvolution.map(toRenderEvolution)}
+            </>
+        )
     }
 
     return (
         <div className="card-detail">
             <img
                 className="card-detail-image"
-                src={pokemon ? `${POKE_API_IMAGE_URL}/versions/generation-v/black-white/animated/${pokemon.id}.gif` : ''}
+                src={pokemon
+                        ? `${POKE_API_IMAGE_URL}/versions/generation-v/black-white/animated/${pokemon.id}.gif`
+                        : '/assets/images/empty-pokemon.png'}
             />
 
             <div className='card-info card-detail-info'>
@@ -70,11 +110,11 @@ const CardDetail: React.FC<CardDetailProps> = ({ pokemon }) => {
                     <div className="card-dimensions">
                         <div className="card-dimension">
                             <h5 className="subtitle">Weight</h5>
-                            <Pill text={`${pokemon.weight / 10} kg`} color="#a9a9a9" />
+                            <Pill text={`${pokemon.weight / 10} kg`} color="#F6F8FC" />
                         </div>
                         <div className="card-dimension">
                             <h5 className="subtitle">Height</h5>
-                            <Pill text={`${pokemon.height} m`} color="#a9a9a9" />
+                            <Pill text={`${pokemon.height} m`} color="#F6F8FC" />
                         </div>
                     </div>
                 }
@@ -87,6 +127,25 @@ const CardDetail: React.FC<CardDetailProps> = ({ pokemon }) => {
                         </div>
                     </div>
                 }
+
+                {pokemon &&
+                    <div className="card-stats">
+                        <h5 className="subtitle">Stats</h5>
+                        <div className="card-stat">
+                            {pokemon.stats.map(toRenderStats)}
+                        </div>
+                    </div>
+                }
+
+                {evolutionChain &&
+                    <div className="card-evolutions-container">
+                        <h5 className="subtitle">Evolution</h5>
+                        <div className="card-evolutions">
+                            {toRenderEvolution(evolutionChain.chain)}
+                        </div>
+                    </div>
+                }
+
             </div>
 
         </div>
--- src/components/card-detail/types.ts
+++ src/components/card-detail/types.ts
@@ -2,4 +2,5 @@ import { PokeApiPokemon } from "../../services/poke-api/types";
 
 export interface CardDetailProps {
     pokemon: PokeApiPokemon | null
+    onClickEvolution: (id: number) => Promise<void> | void
 }
